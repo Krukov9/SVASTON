88b2dcdcb88a00717a757e3e93cee51919aaacd3
Krukov9 <98971678+Krukov9@users.noreply.github.com>
2022-02-07 18:20:20 +0500

    Update Svaston.py

--- Svaston.py
+++ Svaston.py
@@ -1,25 +1,36 @@
 print("""  __                       
  (_      _.  _ _|_  _  ._  
  __) \/ (_| _>  |_ (_) | | """)
-print("[1].Penis")
-
-choose = input(">>>")
-if choose == "1":
-    import pyautogui
-    import time
-    time.sleep(5)
-    pyautogui.click(button='right')
-    pyautogui.moveTo(963,241)
-    pyautogui.click(button='right')
-    pyautogui.moveTo(1280,815)
-    pyautogui.click(button='right')
-    pyautogui.moveTo(335,532)
-    pyautogui.click(button='right')
-    pyautogui.moveTo(1280,249)
-    pyautogui.keyDown('Space')
-    pyautogui.click(button='right')
-    pyautogui.keyUp('Space')
-    pyautogui.PAUSE = 5
-    screenshot = pyautogui.screenshot()
-    screenshot.save('SvastonScreen.png')
-    print("[Penis.minecraft]Succesfully")
+print("[1].Построить член")
+while 1 == 1:
+    choose = input(">>>")
+    if choose == "1":
+        print("[1].построить один раз")
+        print("[2].авто постройка")
+        while 1 == 1:
+            choose = input(">>>")
+            if choose == "1":
+            import pyautogui
+            import time
+            time.sleep(5)
+            pyautogui.click(button='right')
+            pyautogui.keyDown("D")
+            time.sleep(0.25)
+            pyautogui.keyUp("D")
+            pyautogui.click(button='right')
+            time.sleep(0.25)
+            pyautogui.keyDown("A")
+            time.sleep(0.10)
+            pyautogui.keyUp("A")
+            pyautogui.click(button='right')
+            pyautogui.keyDown("Space")
+            time.sleep(0.10)
+            pyautogui.keyUp("Space")
+            pyautogui.click(button='right')
+            pyautogui.keyDown("Space")
+            time.sleep(0.10)
+            pyautogui.keyUp("Space")
+            pyautogui.click(button='right')
+            screenshot = pyautogui.screenshot()
+            screenshot.save('SvastonScreen.png')
+            print("[Penis.minecraft]Succesfully")
